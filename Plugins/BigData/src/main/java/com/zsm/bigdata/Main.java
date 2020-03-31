d12b9701c8938a6c7a2b24a627eda826453d3fe0
zengsm <453752303@qq.com>
2020-03-31 09:47:40 +0800

    添加大数据Storm，Canal组件模块

---
+++ Plugins/BigData/src/main/java/com/zsm/bigdata/Main.java
@@ -0,0 +1,24 @@
+package com.zsm.bigdata;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @Author: zengsm.
+ * @Description:
+ * @Date:Created in 2018/2/6 16:15.
+ * @Modified By:
+ */
+public class Main
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        KafkaConsumer consumer=new KafkaConsumer();
+        consumer.r
+
+    }
+}
---
+++ Plugins/BigData/src/main/java/com/zsm/bigdata/hdfs/HDFSOperator.java
@@ -0,0 +1,12 @@
+package com.zsm.bigdata.hdfs;
+
+/**
+ * @Author: zengsm.
+ * @Description: TODO()
+ * @Date:Created in 2018/8/1.
+ * @Modified By:
+ */
+public class HDFSOperator
+{
+
+}
